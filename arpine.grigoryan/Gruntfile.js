3e9113433f43ef0295e1674cf0d9a308ba9f77fb
arpine.grigoryan <arpine.grigoryan@arm.synisys.com>
2015-04-16 01:00:47 +0400

    edit names

--- arpine.grigoryan/Gruntfile.js
+++ arpine.grigoryan/Gruntfile.js
@@ -1,31 +1,32 @@
 module.exports = function(grunt) {
 grunt.initConfig({
 
-    // Validate js files with jshint.
-    jshint: { 
-		all: ['src/**/*.js']
+   	 // Validate js files with jshint.
+    	jshint: { all: [ 'src/**/*.js']
 	},
 	
 	//Minify files with UglifyJS.
 	uglify: {
 		my_target: {
 		  files: {
-			'minify/output.min.js': ['src/**/*.js']
+			'minify/output.min.js': ['*.js']
 			}
 		}
-	},
-	
+  	},
+
 	jasmine: {
-		sorce : "src/homeWork1.js",
+		src: 'src/scripts/*.js',
 		options: {
-			specs:"spec/spec.js"
+			specs: 'src/tests/*Spec.js'
 		}
 	}
+	
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  
-  grunt.registerTask('default', ['jshint', 'uglify']);
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
 };
---
+++ arpine.grigoryan/src/scripts/calculator.js
@@ -0,0 +1,19 @@
+OperatorEnum = {
+    SUM : {symb : "+", result: function (a,b) { return a+b; }},
+    SUB : {symb : "-", result: function (a,b) { return a-b; }},
+    MUL : {symb : "*", result: function (a,b) { return a*b; }},
+	DIV : {symb : "/", result: function (a,b) { return a/b; }},
+
+	getBySymbol : function (symb) {
+		for (var op in this) {
+			if (symb == this[op].symb) {
+				return this[op];
+			}
+		}
+	} ,
+ 
+	 getResult : function(a,b,symbol) {
+		return this.getBySymbol(symbol).result(a,b);
+	 },
+
+};
