48af9cbb9e903b85566bdf5f058df6c56323dc40
PerchVoskerchyan <p.voskerchyan@gmail.com>
2015-04-16 02:09:04 -0700

    create app

--- perch.voskerchyan/app/js/main.js
+++ perch.voskerchyan/app/js/main.js
@@ -27,7 +27,7 @@ var num2 = 8;
 var opr = '/';
 
 var result = calc(num1, num2, opr);
-var message = "The result of " + num1 + " " + opr + " " + num2 + " = " + result;
+var message = num1 + " " + opr + " " + num2 + " = " + result;
 
 $(document).ready(function(){
 	$(".result").text(message);
